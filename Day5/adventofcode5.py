66aa2c4ff70d26a26f2a808f26fdeb1be970355f
Jan <moep_2007@yahoo.de>
2019-12-11 02:59:10 +0100

    Create adventofcode5.py
    Day 5 has a return of Day 2's automaton. Might just go and rewrite that thing from scratch instead of trying to salvage my code from Day 2.
    
    Fun fun fun! :V

---
+++ Day5/adventofcode5.py
@@ -0,0 +1,9 @@
+def operate(opcode, arg1, arg2):
+    if (opcode==1):
+        return arg1+arg2
+    elif (opcode==2):
+        return arg1*arg2
+    elif (opcode==3):
+        return arg1
+    elif (opcode==4):
+        return arg1
